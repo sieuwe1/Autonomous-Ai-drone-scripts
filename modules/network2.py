d3f9bf763511021e665a5850d1a0ff4576b9e1e6
NIek <niekvandam1@outlook.com>
2021-06-15 15:21:12 +0200

    created own model

--- modules/network2.py
+++ modules/network2.py
@@ -1,5 +1,5 @@
-#network file. Based on donkeycar network.py file. 
-#%%
+# network file. Based on donkeycar network.py file.
+# %%
 
 import os
 import numpy as np
@@ -14,18 +14,20 @@ from tensorflow.python.keras.layers.merge import concatenate
 from tensorflow.python.keras.layers import LSTM
 from tensorflow.python.keras.layers.wrappers import TimeDistributed as TD
 from tensorflow.python.keras.layers import Conv3D, MaxPooling3D, Cropping3D, Conv2DTranspose
- 
+
+
 def create_encoder(img_in):
-    x = img_in 
-    x = Convolution2D(24, (5,5), strides=(2,2), activation='relu')(x)
-    x = Convolution2D(32, (5,5), strides=(2,2), activation='relu')(x)
-    x = Convolution2D(64, (3,3), strides=(2,2), activation='relu')(x)
-    x = Convolution2D(64, (3,3), strides=(1,1), activation='relu')(x)
-    x = Convolution2D(64, (3,3), strides=(1,1), activation='relu')(x)
-    x = Flatten(name='flattened')(x)    
+    x = img_in
+    x = Convolution2D(24, (5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(32, (5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(64, (3, 3), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(64, (3, 3), strides=(1, 1), activation='relu')(x)
+    x = Convolution2D(64, (3, 3), strides=(1, 1), activation='relu')(x)
+    x = Flatten(name='flattened')(x)
     x = Dense(100, activation='relu')(x)
     x = Dropout(.1)(x)
     return x
+
 
 def create_dense(numerical_in):
     y = numerical_in
@@ -34,33 +36,37 @@ def create_dense(numerical_in):
     y = Dense(14, activation='relu')(y)
     return y
 
-def create_model(): #img_right_in,img_left_in
-    input_shape = (300,300,3)
+
+def create_model():  # img_right_in,img_left_in
+    input_shape = (300, 300, 3)
     img_depth_in = Input(shape=input_shape, name='img_depth_in')
-    
+
     path_distance_in = Input(shape=(1,), name="path_distance_in")
 
     x = create_encoder(img_depth_in)
     y = create_dense(path_distance_in)
-    
-    z = concatenate([x,y])
+
+    z = concatenate([x, y])
     z = Dense(50, activation='relu')(z)
     z = Dropout(.1)(z)
     z = Dense(50, activation='relu')(z)
     z = Dropout(.1)(z)
 
     outputs = []  # will be roll, yaw and hight
-    
+
     for i in range(3):
         outputs.append(Dense(1, activation='linear', name='out_' + str(i))(z))
-        
+
     model = Model(inputs=[img_depth_in, path_distance_in], outputs=outputs)
-    
+
     return model
+
 
 model = create_model()
 
 dot_img_file = 'model_1.png'
 tf.keras.utils.plot_model(model, to_file=dot_img_file, show_shapes=True)
 
+# %%
+print("asdf")
 # %%
